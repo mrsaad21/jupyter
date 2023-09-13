ec3b9693219b29e229340001380bf913dd6b34c6
mrsaad21 <126778882+mrsaad21@users.noreply.github.com>
2023-09-13 21:32:24 +0500

    Add files via upload

---
+++ Untitled1.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## Chapter 3
+
+# In[2]:
+
+
+x= 5
+if x > 2:
+    print ('Bigger than 2')
+    print ('Still Bigger')
+print ('Done with 2')
+
+for i in range (5):
+    print(i)
+    if i > 2:
+        print ("Bigger than 2")
+    print ("Done with i",i)
+print("All done")
+
+
+# In[ ]:
+
+
+
+
---
+++ chapter 2 .py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## Chapter 2
+
+# ### Basics of variables 
+
+# In[1]:
+
+
+a = 35.0
+b = 12.50
+c= a*b
+print(c)
+
+
+# ### Type conversions
+
+# In[2]:
+
+
+print (float(99) + 100)
+i=42
+type(i)
+f = float (i)
+print (f)
+type(f)
+
+
+# ### User input
+
+# In[ ]:
+
+
+name = input ('what is your name?')
+print ('Welcome', name)
+
+
+# In[ ]:
+
+
+
+
